b90c0475602d97e3ae14376c6fac6c64b7d69f4b
dave-sloan-123 <dsloan399@yahoo.co.uk>
2026-03-26 04:44:11 +0000

    Add initial Django project structure and core functionality
    - Created manage.py for administrative tasks.
    - Added __init__.py files to mark directories as Python packages.
    - Implemented admin registration for Team, Activity, Leaderboard, and Workout models.
    - Set up ASGI and WSGI configurations for deployment.
    - Developed models for Team, Activity, Leaderboard, and Workout with string representations.
    - Created serializers for User, Team, Activity, Leaderboard, and Workout.
    - Configured Django settings with database connection to MongoDB using Djongo.
    - Implemented test cases for API endpoints.
    - Established URL routing for API views using Django REST framework.
    - Developed viewsets for User, Team, Activity, Leaderboard, and Workout with API root endpoint.
    - Added command to populate the database with test data.

---
+++ octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
@@ -0,0 +1,72 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+from djongo import models
+
+from octofit_tracker import settings
+
+from django.db import connection
+
+class Team(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    class Meta:
+        app_label = 'octofit_tracker'
+
+class Activity(models.Model):
+    name = models.CharField(max_length=100)
+    user = models.CharField(max_length=100)
+    duration = models.IntegerField()
+    class Meta:
+        app_label = 'octofit_tracker'
+
+class Leaderboard(models.Model):
+    team = models.CharField(max_length=100)
+    points = models.IntegerField()
+    class Meta:
+        app_label = 'octofit_tracker'
+
+class Workout(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    class Meta:
+        app_label = 'octofit_tracker'
+
+class Command(BaseCommand):
+    help = 'Populate the octofit_db database with test data'
+
+    def handle(self, *args, **options):
+        User = get_user_model()
+        # Delete all data
+        User.objects.all().delete()
+        Team.objects.all().delete()
+        Activity.objects.all().delete()
+        Leaderboard.objects.all().delete()
+        Workout.objects.all().delete()
+
+        # Create teams
+        marvel = Team.objects.create(name='Marvel')
+        dc = Team.objects.create(name='DC')
+
+        # Create users
+        users = [
+            User.objects.create_user(username='ironman', email='ironman@marvel.com', password='password', first_name='Tony', last_name='Stark'),
+            User.objects.create_user(username='captainamerica', email='cap@marvel.com', password='password', first_name='Steve', last_name='Rogers'),
+            User.objects.create_user(username='batman', email='batman@dc.com', password='password', first_name='Bruce', last_name='Wayne'),
+            User.objects.create_user(username='wonderwoman', email='wonderwoman@dc.com', password='password', first_name='Diana', last_name='Prince'),
+        ]
+
+        # Create activities
+        Activity.objects.create(name='Running', user='ironman', duration=30)
+        Activity.objects.create(name='Swimming', user='batman', duration=45)
+        Activity.objects.create(name='Cycling', user='wonderwoman', duration=60)
+        Activity.objects.create(name='Yoga', user='captainamerica', duration=20)
+
+        # Create leaderboard
+        Leaderboard.objects.create(team='Marvel', points=100)
+        Leaderboard.objects.create(team='DC', points=90)
+
+        # Create workouts
+        Workout.objects.create(name='Pushups', description='Do 3 sets of 15 pushups')
+        Workout.objects.create(name='Situps', description='Do 3 sets of 20 situps')
+        Workout.objects.create(name='Squats', description='Do 3 sets of 25 squats')
+
+        self.stdout.write(self.style.SUCCESS('octofit_db database populated with test data.'))
